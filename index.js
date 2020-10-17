cec4c8ea217462cae1ffecea23d9a660e09ddfee
michael dilulo <snoleopard01001101@gmail.com>
2020-10-17 10:10:30 -0700

    404 response, routes that are not setup

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
+
+//if user hits our page, want them to get a specific response, * is wildcard (all routes)
+server.get('*', (request, response, next) => {
+    response.status(404).send('<h1> 404 SORRY PAGE NOT FOUND </h1>');
+})
+
+
 server.listen(port, () => {
     console.log('server is running on port:', port);
 });
