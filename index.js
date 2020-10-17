8b89f4e73c20e18ab517c55264fb22a1a986c4f3
michael dilulo <snoleopard01001101@gmail.com>
2020-10-17 10:24:33 -0700

    Server is setup

--- index.js
+++ index.js
@@ -2,23 +2,6 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
-
-// Next() - Continue through server request to see if anything else matches
-server.get('/test', (req, res, next) => {
-    console.log('Test Success')
-    next();
-});
-
-
-
-
-// if user hits our page, want them to get a specific response, * is wildcard (all routes)
-// default error handler
-server.get('*', (request, response, next) => {
-    response.status(404).send('<h1> 404 SORRY PAGE NOT FOUND </h1>');
-});
-
-
 server.listen(port, () => {
     console.log('server is running on port:', port);
 });
