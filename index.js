71a054ce9a9a1d9225743a62c51074d24a2f51cd
michael dilulo <snoleopard01001101@gmail.com>
2020-10-17 10:18:46 -0700

    Middleware and 200 status code notes

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const server = express();
 const port = 3000;
 
 
-//if user hits our page, want them to get a specific response, * is wildcard (all routes)
+// MIDDLEWARE how requests are handled when I want to deal with them
+// 200: Good to go
+// How to register routes in between
+server.get('/test', (req, res, next) => {
+    res.status(200).send('Success!')
+});
+
+
+
+
+// if user hits our page, want them to get a specific response, * is wildcard (all routes)
+// default error handler
 server.get('*', (request, response, next) => {
     response.status(404).send('<h1> 404 SORRY PAGE NOT FOUND </h1>');
-})
+});
 
 
 server.listen(port, () => {
